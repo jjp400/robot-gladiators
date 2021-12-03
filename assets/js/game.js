f93ba6e4360bbb2ac0f796724e0c9cd21d929bd3
Jotas <jproodian721@gmail.com>
2021-12-03 11:41:03 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,14 +30,16 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
 
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         )
@@ -56,7 +58,9 @@ var fight = function(enemyName) {
         }
 
         // remove player's health by subtracting the value of 'enemyAttack' from the value of 'playerHealth'
-        playerHealth = playerHealth - enemyAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         )
@@ -84,7 +88,7 @@ var startGame = function() {
             // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
             // if we're not at the last enemy in the array
             if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -178,9 +182,16 @@ var shop = function() {
     }
 };
 
+// var to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+}
+
 startGame();
 
 
-// finished 3.3.5--ready for 3.3.6
+// finished 3.4.3--ready for 3.4.4
 
 // switch statements
